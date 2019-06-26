a18ee4b907d57ce50095ccfa8556c76492439362
cfi <carlo@field.li>
2019-06-26 17:54:23 +0200

    reformat using spaces instead of tabs

--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"flag"
-	"github.com/bwmarrin/discordgo"
-	"go.uber.org/zap"
+  "flag"
+  "github.com/bwmarrin/discordgo"
+  "go.uber.org/zap"
 )
 
 var token string
 var sugar *zap.SugaredLogger
 
 func init() {
-	flag.StringVar(&token, "t", "", "Bot Token")
-	flag.Parse()
+  flag.StringVar(&token, "t", "", "Bot Token")
+  flag.Parse()
 
 }
 
 func main() {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync() // flushes buffer, if any
-	sugar = logger.Sugar()
+  logger, _ := zap.NewProduction()
+  defer logger.Sync() // flushes buffer, if any
+  sugar = logger.Sugar()
 
-	if token == "" {
-		sugar.Fatal("No token provided. Please use -t <bot token>")
-	}
+  if token == "" {
+    sugar.Fatal("No token provided. Please use -t <bot token>")
+  }
 
-	sugar.Info("Starting bot.")
+  sugar.Info("Starting bot.")
 
-	dg, err := discordgo.New("Bot " + token)
-	if err != nil {
-		sugar.Fatalf("Error starting bot: %s", err)
-	}
+  dg, err := discordgo.New("Bot " + token)
+  if err != nil {
+    sugar.Fatalf("Error starting bot: %s", err)
+  }
 
-	dg.AddHandler(onReady)
+  dg.AddHandler(onReady)
 
 }
 
 func onReady(session *discordgo.Session) {
-	sugar.Info("Bot started and listening.")
+  sugar.Info("Bot started and listening.")
 }
 
 func onMessage(session *discordgo.Session, message *discordgo.MessageCreate) {
-	sugar.Debugf("[%s] %s: %s", message.Timestamp, message.Author.Username, message.Content)
-	// todo: message handler
+  sugar.Debugf("[%s] %s: %s", message.Timestamp, message.Author.Username, message.Content)
+  // todo: message handler
 }
