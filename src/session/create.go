bb0ea8dc09f4e6aa5781b45de0775ce895d674a9
Bailey Matthews <baileyjmatthews@gmail.com>
2019-06-27 16:31:56 +0000

    Added invite

--- src/session/create.go
+++ src/session/create.go
@@ -3,7 +3,15 @@ package session
 import(
   "github.com/bwmarrin/discordgo"
   "github.com/finione/Discord-Hack-Week/src/cli"
+  "github.com/finione/Discord-Hack-Week/src/util"
+  "go.uber.org/zap"
 )
+
+var logger *zap.SugaredLogger
+
+func main() {
+  logger := util.GetSugaredLogger()
+}
 
 // Create guild/session function
 func Create(client *discordgo.Session, name string, user *discordgo.User, options cli.Options) (session *Session, error error) {
@@ -17,9 +25,25 @@ func Create(client *discordgo.Session, name string, user *discordgo.User, option
   session.Guild = guild
 
   if error != nil {
+    logger.Errorf("Err: %v\n", error)
     return
   }
+  /** TODO: Move to OAuth2 **/
+  // err := client.GuildMemberAdd(user.Token, session.Guild.ID, user.ID, "", emptyStringMap, false, false)
 
+  channel, error := client.GuildChannelCreate(session.Guild.ID, "Welcome", discordgo.ChannelTypeGuildText)
+  if error != nil {
+    logger.Errorf("Err: %v\n", error)
+    return
+  }
+  invite, error := client.ChannelInviteCreate(channel.ID, discordgo.Invite{Temporary: true, MaxAge: 900000000000, MaxUses: 4})
+  if error != nil {
+    logger.Errorf("Err: %v\n", error)
+  }
+
+  logger.Infof("Please click this link to join your new server: https://discord.gg/%v", invite.Code)
+
+  
   // todo: init guild creation scripts
 
   return
