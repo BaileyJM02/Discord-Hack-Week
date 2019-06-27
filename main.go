19ee6d1a50445f79ef6fc37cb66d0d2dd8ef5ffe
cfi <carlo@field.li>
2019-06-27 08:23:12 +0100

    improve init script to listen to termination and kill signals, actually open websocket connection

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
   "flag"
   "github.com/bwmarrin/discordgo"
   "go.uber.org/zap"
+  "os"
+  "os/signal"
+  "syscall"
 )
 
 var token string
@@ -16,9 +19,10 @@ func init() {
 }
 
 func main() {
-  logger, _ := zap.NewProduction()
+  logger, _ := zap.NewDevelopment()
   defer logger.Sync() // flushes buffer, if any
   sugar = logger.Sugar()
+
 
   if token == "" {
     sugar.Fatal("No token provided. Please use -t <bot token>")
@@ -29,13 +33,24 @@ func main() {
   dg, err := discordgo.New("Bot " + token)
   if err != nil {
     sugar.Fatalf("Error starting bot: %s", err)
+
   }
 
   dg.AddHandler(onReady)
+  dg.AddHandler(onMessage)
+
+  dg.Open()
+
+  logger.Info("Bot started. Press ctrl+c to exit.")
+  sc := make(chan os.Signal, 1)
+  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+  <-sc
+
+  dg.Close()
 
 }
 
-func onReady(session *discordgo.Session) {
+func onReady(session *discordgo.Session, event *discordgo.Ready) {
   sugar.Info("Bot started and listening.")
 }
 
